725377fd7940e5d3dd99413ee02ae0a6cc8bc89a
F1neboy <dominik9112@gmail.com>
2022-01-18 21:27:15 +0100

    Create project

---
+++ src/main/java/pl/pbs/serwisit/SerwisItApplication.java
@@ -0,0 +1,13 @@
+package pl.pbs.serwisit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerwisItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pbs/serwisit/SerwisItApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pbs.serwisit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwisItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
