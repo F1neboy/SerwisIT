9d46f1b6b39552076f4f512db23b3c483a1d89d8
F1neboy <dominik9112@gmail.com>
2022-02-01 22:05:39 +0100

    Site to angular added

--- src/main/resources/frontend/angular-app/src/app/app-routing.module.ts
+++ src/main/resources/frontend/angular-app/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {NaprawaComponent} from "./naprawa/naprawa.component";
+import {StatusComponent} from "./status/status.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'naprawa', component: NaprawaComponent},
+  {path: 'status', component: StatusComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'top'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/main/resources/frontend/angular-app/src/app/app.component.ts
+++ src/main/resources/frontend/angular-app/src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+  loadScripts() {
+    const dynamicScripts = [
+      "../assets/js/jquery.min.js",
+      "./assets/js/jquery.scrollex.min.js",
+      "./assets/js/jquery.scrolly.min.js",
+      "./assets/js/browser.min.js",
+      "./assets/js/breakpoints.min.js",
+      "./assets/js/util.js",
+      "./assets/js/main.js"
+    ];
+    for (let i = 0; i < dynamicScripts.length; i++) {
+      const node = document.createElement('script');
+      node.src = dynamicScripts[i];
+      node.type = 'text/javascript';
+      node.async = false;
+      node.charset = 'utf-8';
+      document.getElementsByTagName('head')[0].appendChild(node);
+    }
+  }
+  constructor() {
+    this.loadScripts();
+  }
 }
